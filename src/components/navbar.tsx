e5abc872f9a79a0cd5ffcb98e00cd1b19f2dcb22
juliscapucin <jscapucin@gmail.com>
2026-01-05 18:07:16 +0000

    setup: add logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import { Suspense } from "react";
 import { AuthButton } from "@/app/auth/_components/auth-button";
+import Logo from "./logo";
 
 export default function Navbar() {
   return (
     <nav className="fixed left-0 top-0 right-0 flex justify-center border-b border-b-foreground/10 h-header z-50 bg-primary">
       <div className="w-full max-w-desktop flex justify-between items-center p-3 px-5 text-body-tablet">
         <Link href={"/"} className="uppercase font-bold">
-          Smalls.London
+          <Logo />
         </Link>
         <Link href={"/businesses"} className="uppercase">
           Explore
