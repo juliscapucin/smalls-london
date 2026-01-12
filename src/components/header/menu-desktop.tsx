e5ff5806b2c481f607b6b76229eeae8864b61012
juliscapucin <jscapucin@gmail.com>
2026-01-12 14:30:43 +0000

    feat/navbar: add IconMenuIndicator component and integrate it into MenuDesktop

--- src/components/header/menu-desktop.tsx
+++ src/components/header/menu-desktop.tsx
@@ -5,6 +5,7 @@ import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import { AuthButton } from "@/app/auth/_components/auth-button";
 import { Logo } from "@/components/logo";
 import { IconChevronDown } from "@/components/icons/icon-chevron-down";
+import { IconMenuIndicator } from "@/components/icons/icon-menu-indicator";
 import { SearchInput } from "./search-input";
 import type { NavItem } from "./header";
 
@@ -68,8 +69,9 @@ export async function MenuDesktop({ navItems }: MenuDesktopProps) {
             <SearchInput />
           </NavigationMenu.Content>
         </NavigationMenu.Item>
-
-        <NavigationMenuIndicator />
+        <NavigationMenu.Indicator className={classnames.indicator}>
+          <IconMenuIndicator />
+        </NavigationMenu.Indicator>
       </NavigationMenu.List>
 
       <React.Suspense>
@@ -102,27 +104,3 @@ const ListItem = React.forwardRef<HTMLAnchorElement, ListItemProps>(
     </li>
   )
 );
-
-const NavigationMenuIndicator = () => {
-  return (
-    <NavigationMenu.Indicator className={classnames.indicator}>
-      <svg
-        width="23"
-        height="14"
-        viewBox="0 0 23 14"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M11.1504 2.31738L21.1513 11.3174C21.1513 11.3174 21.1504 11.8174 21.1504 13.8174C18.6504 13.8174 5.15039 13.8174 1.15039 13.8174C1.1505 11.352 1.15131 11.3174 1.15131 11.3174L11.1504 2.31738Z"
-          fill="#F5F3EA"
-        />
-        <path
-          d="M21.6504 10.3174L11.1504 1.31738L0.650391 10.3174"
-          stroke="#302634"
-          stroke-width="2"
-        />
-      </svg>
-    </NavigationMenu.Indicator>
-  );
-};
---
+++ src/components/icons/icon-menu-indicator.tsx
@@ -0,0 +1,21 @@
+export const IconMenuIndicator = () => {
+  return (
+    <svg
+      width="23"
+      height="14"
+      viewBox="0 0 23 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.1504 2.31738L21.1513 11.3174C21.1513 11.3174 21.1504 11.8174 21.1504 13.8174C18.6504 13.8174 5.15039 13.8174 1.15039 13.8174C1.1505 11.352 1.15131 11.3174 1.15131 11.3174L11.1504 2.31738Z"
+        fill="var(--background)"
+      />
+      <path
+        d="M21.6504 10.3174L11.1504 1.31738L0.650391 10.3174"
+        stroke="var(--foreground)"
+        stroke-width="2"
+      />
+    </svg>
+  );
+};
